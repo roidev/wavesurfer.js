731677f0088aeb592dbb2e6a6687682ba7e5d6d7
E.Vila <ernest.vila@akinda.ch>
2022-03-30 13:52:22 +0200

    modified the key for drag enable

--- src/plugin/regions/region.js
+++ src/plugin/regions/region.js
@@ -20,11 +20,10 @@ export class Region {
         this.id = params.id == null ? ws.util.getId() : params.id;
         this.start = Number(params.start) || 0;
         this.end =
-            params.end == null
-                ? // small marker-like region
-                this.start +
-                (4 / this.wrapper.scrollWidth) * this.wavesurfer.getDuration()
-                : Number(params.end);
+            params.end == null ? // small marker-like region
+            this.start +
+            (4 / this.wrapper.scrollWidth) * this.wavesurfer.getDuration() :
+            Number(params.end);
         this.resize =
             params.resize === undefined ? true : Boolean(params.resize);
         this.drag = params.drag === undefined ? true : Boolean(params.drag);
@@ -43,7 +42,7 @@ export class Region {
         this.handleRightEl = null;
         this.data = params.data || {};
         this.attributes = params.attributes || {};
-        this.showTooltip = params.showTooltip ?? true;
+        this.showTooltip = params.showTooltip ? ? true;
 
         this.maxLength = params.maxLength;
         // It assumes the minLength parameter value, or the regionsMinLength parameter value, if the first one not provided
@@ -55,9 +54,9 @@ export class Region {
         this.scrollThreshold = params.scrollThreshold || 10;
         // Determines whether the context menu is prevented from being opened.
         this.preventContextMenu =
-            params.preventContextMenu === undefined
-                ? false
-                : Boolean(params.preventContextMenu);
+            params.preventContextMenu === undefined ?
+            false :
+            Boolean(params.preventContextMenu);
 
         // select channel ID to set region
         let channelIdx =
@@ -68,9 +67,9 @@ export class Region {
 
         if (channelIdx !== -1) {
             let channelCount =
-                this.wavesurfer.backend.buffer != null
-                    ? this.wavesurfer.backend.buffer.numberOfChannels
-                    : -1;
+                this.wavesurfer.backend.buffer != null ?
+                this.wavesurfer.backend.buffer.numberOfChannels :
+                -1;
             if (channelCount >= 0 && channelIdx < channelCount) {
                 this.regionHeight = Math.floor((1 / channelCount) * 100) + '%';
                 this.marginTop =
@@ -222,21 +221,23 @@ export class Region {
 
             // Merge CSS properties per handle.
             const handleLeftCss =
-                this.handleStyle.left !== 'none'
-                    ? Object.assign(
-                        { left: '0px' },
-                        css,
-                        this.handleStyle.left
-                    )
-                    : null;
+                this.handleStyle.left !== 'none' ?
+                Object.assign({
+                        left: '0px'
+                    },
+                    css,
+                    this.handleStyle.left
+                ) :
+                null;
             const handleRightCss =
-                this.handleStyle.right !== 'none'
-                    ? Object.assign(
-                        { right: '0px' },
-                        css,
-                        this.handleStyle.right
-                    )
-                    : null;
+                this.handleStyle.right !== 'none' ?
+                Object.assign({
+                        right: '0px'
+                    },
+                    css,
+                    this.handleStyle.right
+                ) :
+                null;
 
             if (handleLeftCss) {
                 this.style(this.handleLeftEl, handleLeftCss);
@@ -256,12 +257,10 @@ export class Region {
             return this.formatTimeCallback(start, end);
         }
         return (start == end ? [start] : [start, end])
-            .map((time) =>
-                [
-                    Math.floor((time % 3600) / 60), // minutes
-                    ('00' + Math.floor(time % 60)).slice(-2) // seconds
-                ].join(':')
-            )
+            .map((time) => [
+                Math.floor((time % 3600) / 60), // minutes
+                ('00' + Math.floor(time % 60)).slice(-2) // seconds
+            ].join(':'))
             .join('-');
     }
 
@@ -554,13 +553,17 @@ export class Region {
             this.isDragging = false;
             if (event.target.tagName.toLowerCase() === 'handle') {
                 this.isResizing = true;
-                resize = event.target.classList.contains('wavesurfer-handle-start')
-                    ? 'start'
-                    : 'end';
-            } else {
+                resize = event.target.classList.contains('wavesurfer-handle-start') ?
+                    'start' :
+                    'end';
+            } else if (e.shiftKey) {
                 this.isDragging = true;
                 drag = true;
                 resize = false;
+            } else {
+                // this.isDragging = true;
+                // drag = true;
+                // resize = false;
             }
         };
         const onUp = (event) => {
@@ -681,7 +684,9 @@ export class Region {
         this.element.addEventListener('touchstart', onDown);
 
         document.body.addEventListener('mousemove', onMove);
-        document.body.addEventListener('touchmove', onMove, {passive: false});
+        document.body.addEventListener('touchmove', onMove, {
+            passive: false
+        });
 
         document.addEventListener('mouseup', onUp);
         document.body.addEventListener('touchend', onUp);
@@ -802,7 +807,11 @@ export class Region {
             cursorStyle = 'auto';
         }
 
-        this.handleLeftEl && this.style(this.handleLeftEl, { cursor: cursorStyle });
-        this.handleRightEl && this.style(this.handleRightEl, { cursor: cursorStyle });
+        this.handleLeftEl && this.style(this.handleLeftEl, {
+            cursor: cursorStyle
+        });
+        this.handleRightEl && this.style(this.handleRightEl, {
+            cursor: cursorStyle
+        });
     }
 }
